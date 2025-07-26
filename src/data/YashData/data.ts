673140a96700a0234a4410642fb223119a154e54
kushagra2503 <yashyyy831@gmail.com>
2025-07-26 17:04:06 +0530

    made about section for yash role

---
+++ src/data/YashData/data.ts
@@ -0,0 +1,98 @@
+import { SiBuymeacoffee } from "react-icons/si";
+import { FaXTwitter, FaGithub, FaDiscord } from "react-icons/fa6";
+
+const yashImage = "/assets/Images/pfps/sung.gif";
+const yashName = "Yash";
+const yashAbout = `
+  <p>
+    Hey, I'm Gruz, a full-stack developer with OCPD. I love building things that people genuinely use and love, not just for showcases (like the typical engineer).
+  </p>
+  <p>
+    Besides programming, I'm a volleyball player who loves football. I also enjoy playing video games, mostly FIFA and Valorant in my free time.
+  </p>
+  <p>
+    If you have an idea and would like to collaborate, feel free to DM me! I'm always open and ready to connect and work on exciting projects. Also, be sure to check out my other side, just click on the navbar image!
+  </p>
+`
+const yashbio = "loda lehsun"
+
+const yashLnk = [
+    {
+        id: 1,
+        name: "Github",
+        link: "https://github.com/kushagra2503",
+        icon: FaGithub,
+    },
+    {
+        id: 2,
+        name: "Twitter",
+        link:  "https://x.com/intent/follow?screen_name=Brainfdev",
+        icon: FaGithub,
+    },
+    {
+        id: 3,
+        name: "Discord",
+        link: "https://discord.com/users/1000000000000000000",
+        icon: FaDiscord,
+    },
+    {
+        id: 4,
+        name: "Buy me a coffee",
+        link: "https://www.buymeacoffee.com/kushagra2503",
+        icon: SiBuymeacoffee,
+    },
+]
+
+const yashFooterLInk = [
+    {
+        id: 1,
+        name: "Github",
+        link: "",
+        icon: FaGithub,
+    },
+    {
+        id: 2,
+        name: "Twitter",
+        link: "https://x.com/intent/follow?screen_name=Brainfdev",
+        icon: FaXTwitter,
+    },
+    {
+        id: 3,
+        name: "Discord",
+        link: "https://discord.com/users/1000000000000000000",
+        icon: FaDiscord,
+    },
+    {
+        id: 4,
+        name: "Buy me a coffee",
+        link: "https://www.buymeacoffee.com/kushagra2503",
+        icon: SiBuymeacoffee,
+    },
+]
+
+const yashContact =  "Most of the time, you'll find me hanging out on my Discord channel while coding. I'm also super active on X, so feel free to DM me there!"
+const yashContactLink = [
+    {
+        id: 1,
+        name: "Discord",
+        link: "https://discord.com/users/1000000000000000000",
+        icon: FaDiscord,
+    },
+    {
+        id: 2,
+        name: "Twitter",
+        link: "https://x.com/intent/follow?screen_name=Brainfdev",
+        icon: FaXTwitter,
+    },
+]
+
+export {
+    yashImage,
+    yashName,
+    yashAbout,
+    yashbio,
+    yashLnk,
+    yashFooterLInk,
+    yashContact,
+    yashContactLink
+}
