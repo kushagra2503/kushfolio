aaa6743ed01436a2072488a39b1e6a6be2b5284a
kushagra2503 <yashyyy831@gmail.com>
2025-07-28 11:55:13 +0530

    added some new components in page component folder

---
+++ src/components/PageComponent/ReachOut.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { yashContact, yashContactLink } from "@/data/YashData/data"
+import SectionTitle from "../SectionTitle"
+import { useSwitch } from "../Context/SwitchContext"
+import { kushagraContact, kushagraContactLink } from "@/data/KushagraData/data"
+
+const ReachOut = () => {
+    const { isSwitchOn } = useSwitch()
+    const contactLink = isSwitchOn ? yashContactLink : kushagraContactLink
+
+    return (
+        <section className="flex flex-col gap-0.5">
+            <SectionTitle title="Reach out to me" />
+            <div className="flex flex-col gap-2.5">
+                <p>{isSwitchOn ? yashContact : kushagraContact}</p>
+                <div>
+                    <div className="flex flex-wrap items-center gap-2">
+                        {contactLink.map((link) => (
+                            <a className="btn" key={link.id} target="_blank " href={link.link}>
+                                <link.icon/>
+                                {link.name}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ReachOut;
