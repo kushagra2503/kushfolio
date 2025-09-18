a745a5b5e52b130330b3cf63b142c226ed9d35a1
kushagra2503 <yashyyy831@gmail.com>
2025-09-18 14:43:22 +0530

    style(Projects): update text to indicate upcoming changes

--- src/components/PageComponent/Projects.tsx
+++ src/components/PageComponent/Projects.tsx
@@ -2,7 +2,7 @@ const Projects = () => {
   return (
     <div>
       <h2 className="text-xl font-bold mb-4 text-light-text dark:text-mainWhite">Projects & Contributions</h2>
-      <p className="text-light-muted dark:text-mainWhite/70">Updating soon</p>
+      <p className="text-light-muted dark:text-mainWhite/70">Updating soon..</p>
     </div>
   )
 }
